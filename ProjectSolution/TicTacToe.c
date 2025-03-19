5d398a2420b724d496328a50c436f68ad2009b94
SethATU <g00467621@atu.ie>
2025-03-19 19:37:04 +0000

    update

--- ProjectSolution/TicTacToe.c
+++ ProjectSolution/TicTacToe.c
@@ -44,14 +44,23 @@ int main()
 				boardSetUp();
 				freeSpace();
 				movePlayer();
+				if (checkWin() != ' ')
+				{
+					break;
+				}
 
 				//cpus move
 				boardSetUp();
 				freeSpace();
 				moveComputer();
+				if (checkWin() != ' ')
+				{
+					break;
+				}
 
 			} while (freeSpace() != 0);
 
+			boardSetUp();
 			//anounces if the cpu or the player won 
 			printWin();
 			break;
@@ -117,7 +126,7 @@ void movePlayer()
 	scanf_s("%d", &x);
 	
 	//player enters colume
-	printf("\nEnter Colume (0-2): ");
+	printf("Enter Colume (0-2): ");
 	scanf_s("%d", &y);
 
 	//if the player enters a number to big the player is asked to reenter their move
@@ -126,12 +135,60 @@ void movePlayer()
 		printf("\nInvalid entry!!");
 		movePlayer();
 	}
-	return board[x][y];
+	else if (board[x][y] != ' ')
+	{
+		printf("\nInvalid entry!!");
+		movePlayer();
+	}
+
+	board[x][y] = 'X';
 }
 
 char checkWin()
 {
+	//checking for a rown win 
+	if (board[0][0] == board[0][1] && board[0][0] == board[0][2])
+	{
+		return board[0][0];
+	}
+	else if (board[1][0] == board[1][1] && board[1][0] == board[1][2])
+	{
+		return board[1][0];
+	}
+	else if (board[2][0] == board[2][1] && board[2][0] == board[2][2])
+	{
+		return board[2][0];
+	}
 
+	//checking for diagional win
+	else if (board[0][2] == board[1][1] && board[0][2] == board[2][0])
+	{
+		return board[0][2];
+	}
+	else if (board[0][0] == board[1][1] && board[0][0] == board[2][2])
+	{
+		return board[0][0];
+	}
+
+	//checking for columb win
+	if (board[0][0] == board[1][0] && board[0][0] == board[2][0])
+	{
+		return board[0][0];
+	}
+	else if (board[0][1] == board[1][1] && board[0][1] == board[2][1])
+	{
+		return board[0][1];
+	}
+	else if (board[0][2] == board[1][2] && board[0][2] == board[2][2])
+	{
+		return board[0][2];
+	}
+
+	//if non of the if statments are true then its a tie
+	else
+	{
+		return ' ';
+	}
 }
 
 void printWin()
