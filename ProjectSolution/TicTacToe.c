b77855d948a57f0c74832c4ef5b81f99b63f1b2e
SethATU <g00467621@atu.ie>
2025-03-19 12:39:14 +0000

    Adding files

---
+++ ProjectSolution/TicTacToe.c
@@ -0,0 +1,23 @@
+//C++ Mini Project 
+//TicTaCToe
+
+#include <stdio.h>
+#include <stdlib.h> //used for rand() and srand()
+#include <time.h>	//used for time()
+
+/*
+inisalise board
+win conditions
+winner message
+my go
+computers go
+board updates
+check for clear spaces
+reset board
+*/
+
+
+int main()
+{
+	return 0;
+}
